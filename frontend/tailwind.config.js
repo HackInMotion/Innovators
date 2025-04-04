08dcb135e34d73d247ac68e2be8f4278747e8843
Subodh Rana <subodhrana390@gmail.com>
2025-04-04 14:11:03 +0530

    carousel

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-pattern": "url('/src/assets/Hero.avif')",
+      },
+    },
   },
   plugins: [],
 };
