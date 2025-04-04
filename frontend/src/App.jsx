f7cedb5d1ca84c6a6a86d4a88e4e567fc4aec924
Subodh Rana <subodhrana390@gmail.com>
2025-04-05 01:47:13 +0530

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ClientRoute from "./pages/ClientRoute";
 import LoginPage from "./pages/LoginPage";
 import SignupPage from "./pages/SignupPage";
-
+import BotpressChat from './components/BotPress'
 
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/sign-up" element={<SignupPage />} />
       </Routes>
+      <BotpressChat />
     </Router>
   );
 }
