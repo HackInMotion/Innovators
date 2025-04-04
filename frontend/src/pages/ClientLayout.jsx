21e62f2823964a6e37f7da929e65330259a28df5
Subodh Rana <subodhrana390@gmail.com>
2025-04-04 12:40:05 +0530

    router added

---
+++ frontend/src/pages/ClientLayout.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { Outlet, useLocation } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const ClientLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      {/* Header */}
+      <Header />
+
+      {/* Main Content */}
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
+};
+
+export default ClientLayout;
