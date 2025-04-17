ed71f937e63777d93d7b7b728052fe0797ecdd3a
Subodh Rana <40374844+Subodhrana390@users.noreply.github.com>
2025-04-17 16:15:23 +0530

    Delete frontend directory

--- frontend/src/apiClient/apiClient.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-// Create an Axios instance
-const apiClient = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
-  timeout: 10000,
-});
-
-export default apiClient;
